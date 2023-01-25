f01eb74c4a9dbc570f51b903128ec113c52021eb
Kay-Michael Voit <kay-michael.voit@caesar.de>
2023-01-25 10:29:40 +0100

    v1.1.5 release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="bbo-multitrackpy",
-    version="1.1.4",
+    version="1.1.5",
     description="Detect head positions from MTT files",
     long_description=README,
     long_description_content_type="text/markdown",
